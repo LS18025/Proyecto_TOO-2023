b70487ed231c319fae3a53fdc726ef23090f7426
OscarinLS <oscaralexanderlopezsalguero@gmail.com>
2023-10-23 16:25:05 -0600

    commit inicial
    si

---
+++ PROYECTO-TOO/Gestion_Proyectos/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gestion_Proyectos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult VistaProyecto()
+        {
+            ViewBag.Message = "Gestión de Proyectos";
+
+            return View();
+        }
+    }
+}
