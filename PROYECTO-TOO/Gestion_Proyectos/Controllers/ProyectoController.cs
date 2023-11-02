8d30798fc0d59235ce4bb174e9adb897147efa6a
OscarinLS <oscaralexanderlopezsalguero@gmail.com>
2023-11-02 13:55:11 -0600

    Reconocimiento de proyecto por idUsuario

--- PROYECTO-TOO/Gestion_Proyectos/Controllers/ProyectoController.cs
+++ PROYECTO-TOO/Gestion_Proyectos/Controllers/ProyectoController.cs
@@ -33,10 +33,25 @@ namespace Gestion_Proyectos.Controllers
         [HttpGet]
         public JsonResult ListarProyectos()
         {
-            List<Proyecto> listaProyectos = new List<Proyecto>();
-            listaProyectos = new GestionProyectos().GetProyectos();
-            return Json(new { data = listaProyectos }, JsonRequestBehavior.AllowGet);
+            if (Session["usuario"] != null)
+            {
+                Usuario usuario = (Usuario)Session["usuario"];
+                List<Proyecto> listaProyectos = ObtenerProyectosPorUsuario(usuario.id);
+
+                return Json(new { data = listaProyectos }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                // Si el usuario no ha iniciado sesión, retorna un JSON vacío o un mensaje de error.
+                return Json(new { data = new List<Proyecto>() }, JsonRequestBehavior.AllowGet);
+            }
         }
+
+        private List<Proyecto> ObtenerProyectosPorUsuario(int usuarioId)
+        {
+            return new GestionProyectos().GetProyectos(usuarioId);
+        }
+
 
         [HttpPost]
         public JsonResult AgregarProyecto(Proyecto proyecto)
@@ -45,15 +60,28 @@ namespace Gestion_Proyectos.Controllers
             {
                 GestionProyectos gestionProyectos = new GestionProyectos();
 
-                bool insercionExitosa = gestionProyectos.newProyecto(proyecto);
+                // Obtén el usuario actual de la sesión
+                Usuario usuarioActual = (Usuario)Session["usuario"];
 
-                if (insercionExitosa)
+                if (usuarioActual != null)
                 {
-                    return Json(new { success = true, message = "Proyecto insertado exitosamente" });
+                    // Asigna el ID del usuario al proyecto
+                    
+
+                    bool insercionExitosa = gestionProyectos.newProyecto(proyecto, usuarioActual.id);
+
+                    if (insercionExitosa)
+                    {
+                        return Json(new { success = true, message = "Proyecto insertado exitosamente" });
+                    }
+                    else
+                    {
+                        return Json(new { success = false, message = "Error al insertar el proyecto" });
+                    }
                 }
                 else
                 {
-                    return Json(new { success = false, message = "Error al insertar el proyecto" });
+                    return Json(new { success = false, message = "Usuario no encontrado en la sesión" });
                 }
             }
             else
@@ -61,6 +89,7 @@ namespace Gestion_Proyectos.Controllers
                 return Json(new { success = false, message = "Por favor, complete todos los campos obligatorios correctamente" });
             }
         }
+
         [HttpPost]
         public JsonResult EditarProyecto(Proyecto proyecto)
         {
